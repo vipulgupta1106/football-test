0d637eb17af48701d70122276b620427bc30c41c
Vipul Kumar <vipul.kumar@irissoftware.com>
2021-01-09 15:09:20 +0530

    initial commit

---
+++ src/main/java/com/sapient/footballmatchtest/FootballMatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.footballmatchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballMatchTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/footballmatchtest/FootballMatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.footballmatchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
