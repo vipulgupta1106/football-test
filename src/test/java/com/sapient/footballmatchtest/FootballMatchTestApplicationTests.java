a3411374d0d2d4fc50b5d807fabad1e0e5a6155d
Vipul Kumar <vipul.kumar@irissoftware.com>
2021-01-11 16:40:32 +0530

    added test case

--- src/test/java/com/sapient/footballmatchtest/FootballMatchTestApplicationTests.java
+++ src/test/java/com/sapient/footballmatchtest/FootballMatchTestApplicationTests.java
@@ -2,8 +2,16 @@ package com.sapient.footballmatchtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {"server.port=0", "management.server.port=0"})
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles(profiles = "test")
+@DirtiesContext
 class FootballMatchTestApplicationTests {
 
 	@Test
